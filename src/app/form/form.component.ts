b3d491a2a1dc247770010dff621a2a7e81c9c0a4
Inès Hammami <ines-melya@hotmail.fr>
2021-09-25 20:26:10 +0200

    adding json stringify

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+
+  registerForm: FormGroup = new FormGroup({});
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm= this.formBuilder.group({
+      Civilite:['',[Validators.required]],
+      Nom: ['',[Validators.required, Validators.pattern(/^[a-zA-Z ,.'-]{1,}/)]],
+      Prenom: ['',[Validators.required, Validators.pattern(/^[a-zA-Z ,.'-]{1,}/)]],
+      Login: ['',[Validators.required, Validators.pattern(/^[a-zA-Z ,.'-]{1,}/)]],
+      email: ['',[Validators.required, Validators.pattern(/^[a-zA-Z ,.'-]{1,}/)]],
+      password: ['',[Validators.required, Validators.pattern(/^[a-zA-Z ,.'-]{1,}/)]],
+
+
+    });
   }
+  
+  // convenience getter for easy access to form fields
+get f() { return this.registerForm.controls; }
+
+
+Nom = new FormControl('', Validators.required);
+onSubmit() {
+    console.log('hello');
+    console.log(this.f.Nom);
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+        return;
+    }
+    
+    console.log('valeurs: ', JSON.stringify(this.registerForm.value));
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+}
+
+onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
+}
 
 }
+
+
