1d3c62c321af29787b454a4c8c3855f55bea1c36
Inès Hammami <ines-melya@hotmail.fr>
2021-10-14 13:10:52 +0200

    adding the search bar

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Component, OnInit, Input, NgModule } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { Router } from '@angular/router';
+import { Routes } from '@angular/router';
+import { AppComponent } from '../app.component';
+import { User } from '../Models/User.model';
+import { TestingDirective } from './testing.directive';
+
 
 
 @Component({
@@ -7,51 +15,73 @@ import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angula
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
 
 
-  registerForm: FormGroup = new FormGroup({});
+export class FormComponent  implements OnInit{
+
+  val: string = 'valeur';
+  Civilite : string = "";
+  Nom : string = "";
+  Prenom : string = "";
+  Pays : string = "";
+  Email : string = "";
+  Login : string = "";
+  Adresse : string = "";
+  CP : string = "";
+  Ville : string = "";
+  Telephone :  string = "";
+  Password : string = "";
+
+  registerForm: FormGroup = new FormGroup({Nom: new FormControl('', [Validators.required, Validators.pattern(/[a-zA-Z ,.'-]{3,}/)]),});
   submitted = false;
-
-  constructor(private formBuilder: FormBuilder) { }
+  valid : boolean = false;
 
   ngOnInit(): void {
     this.registerForm= this.formBuilder.group({
-      Civilite:['',[Validators.required]],
-      Nom: ['',[Validators.required, Validators.pattern(/^[a-zA-Z ,.'-]{1,}/)]],
+      //Civilite:['',[Validators.required]],
+      Nom: ['',[Validators.required, Validators.pattern(/[a-zA-Z ,.'-]{3,}/)]],
       Prenom: ['',[Validators.required, Validators.pattern(/^[a-zA-Z ,.'-]{1,}/)]],
       Login: ['',[Validators.required, Validators.pattern(/^[a-zA-Z ,.'-]{1,}/)]],
       email: ['',[Validators.required, Validators.pattern(/^[a-zA-Z ,.'-]{1,}/)]],
       password: ['',[Validators.required, Validators.pattern(/^[a-zA-Z ,.'-]{1,}/)]],
 
 
-    });
+    }); 
   }
-  
-  // convenience getter for easy access to form fields
+  getColor(): string {
+    return 'red';
+  }
+
+  validation() {
+    return true;
+  }
+   // convenience getter for easy access to form fields
 get f() { return this.registerForm.controls; }
+  constructor(private formBuilder: FormBuilder) { }
 
 
-Nom = new FormControl('', Validators.required);
+
 onSubmit() {
     console.log('hello');
-    console.log(this.f.Nom);
-    this.submitted = true;
+    
+    //this.submitted = true;
+    
 
     // stop here if form is invalid
-    if (this.registerForm.invalid) {
-        return;
-    }
-    
-    console.log('valeurs: ', JSON.stringify(this.registerForm.value));
-
-    // display form values on success
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+   // if (this.registerForm.invalid) {
+     //   return;
+   // }
+  
+   
+    this.valid =true;
+    //alert('Récapitulatif\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+    console.log(this.valid);
+  
 }
 
 onReset() {
-    this.submitted = false;
-    this.registerForm.reset();
+    //this.submitted = false;
+    //this.registerForm.reset();
 }
 
 }
--- src/app/validation/validation.component.ts
+++ src/app/validation/validation.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+
+//import { ControlFormService } from '../control-form.service';
+//import { User } from '../Models/User.model';
+
+
+
+
 
 @Component({
   selector: 'app-validation',
   templateUrl: './validation.component.html',
   styleUrls: ['./validation.component.css']
 })
-export class ValidationComponent implements OnInit {
+export class ValidationComponent implements OnInit{
 
+  @Input() valid : boolean = false;
+
+  @Input() Nom : string = "";
+  @Input() Prenom : string = "";
+  @Input() Pays : string = "";
+
+ 
   constructor() { }
+  
+ 
 
-  ngOnInit(): void {
+  ngOnInit():void  {
+  
+ 
   }
 
+ 
 }
