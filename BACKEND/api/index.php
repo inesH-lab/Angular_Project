13e4ba6fc8372eafb0dce94e79806d2badb34889
ines <ines-melya@hotmail.fr>
2022-01-19 18:29:48 +0100

    trying to add bootstrap.php

--- BACKEND/api/index.php
+++ BACKEND/api/index.php
@@ -10,7 +10,7 @@ const JWT_SECRET = "inesines";
 
 require_once "../vendor/autoload.php";
 
-//require_once "/../../bootstrap.php";
+require_once "../../bootstrap.php";
 
 $options = [
     "attribute" => "token",
