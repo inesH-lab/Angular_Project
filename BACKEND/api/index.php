5b447ae7a4c9c6f4af493c814a5d8b9153556b83
ines <ines-melya@hotmail.fr>
2022-01-19 17:51:42 +0100

    try fix config error

--- BACKEND/api/index.php
+++ BACKEND/api/index.php
@@ -10,7 +10,7 @@ const JWT_SECRET = "inesines";
 
 require_once "../vendor/autoload.php";
 
-//require __DIR__. '/../../bootstrap.php';
+require '/../../bootstrap.php';
 
 $options = [
     "attribute" => "token",
